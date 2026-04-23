d2ba30f9f8f0a00d9ab81c51d08d0b0fb9ada971
Takuya Yoshioka <88071178+yoshioka1128@users.noreply.github.com>
2026-04-24 03:59:39 +0900

    modified:   src/core/optimizer.py 	modified:   src/domain/cost/power_cost.py 	deleted:    src/domain/cost/power_cost_func.py 	modified:   src/domain/loss/power_loss.py 	modified:   src/infra/result_handler.py

--- src/domain/cost/power_cost_func.py
+++
@@ -1,9 +0,0 @@
-# power_cost_func.py
-def power_cost(J, h, spin_config):
-    J_sym = J + J.T
-    interaction_term = 0.5 * float(spin_config @ J_sym @ spin_config)
-
-    node_term = float(spin_config @ h)
-
-    energy = interaction_term + node_term
-    return energy
