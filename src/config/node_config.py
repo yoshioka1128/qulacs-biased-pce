49a6614c52a6a8809f050dfefe395b77fd513a6d
Takuya Yoshioka <88071178+yoshioka1128@users.noreply.github.com>
2026-04-27 03:20:21 +0900

    src

--- src/config/node_config.py
+++ src/config/node_config.py
@@ -27,47 +27,106 @@ class NodeConfig:
 # Configuration
 # =========================
 NODE_CONFIG = {
-    60: NodeConfig(
-        n_qubits=6, k=3, ninit=5, iinit=3, imax0=2,
-        alphasc=0.1, chbetaiinit=[1, 0, 3],
-        beta=0.1, depth=5, strbp="_backprop",
-        calpha=6, bound=0.1, nseed=39, subcounts=4001
-    ),
-
-    210: NodeConfig(
-        n_qubits=8, k=4, ninit=5, iinit=2, imax0=2,
-        alphasc=0.1, chbetaiinit=[0, 4, 2],
-        beta=0.1, depth=5, strbp="_backprop",
-        calpha=6, bound=0.1, nseed=0, subcounts=4001
-    ),
-
-    18: NodeConfig(
-        n_qubits=4, k=2, ninit=5, iinit=3, imax0=1,
-        alphasc=1.5, chbetaiinit=[2, 3, 3, 4],
+    (18, 0.1, "nobias"): NodeConfig(
+        alphasc=2.5, beta=0.1, iinit=4,
+        n_qubits=4, k=2, ninit=5, imax0=1,
+        chbetaiinit=[2, 3, 3, 4],
         betas=[-0.1, 0.0, 0.1, 0.2],
-        beta=0.1, depth=5, strbp="_backprop",
+        depth=5, strbp="_backprop",
         calpha=80, bound=1.0, nseed=24, subcounts=4001
     ),
 
-    756: NodeConfig(
-        n_qubits=10, k=5, ninit=5, iinit=2, imax0=3,
-        alphasc=0.5,
-        beta=0.0, depth=5, strbp="_backprop",
+    (60, 0.1, "nobias"): NodeConfig(
+        alphasc=6.0, beta=0.1, iinit=4, 
+        n_qubits=6, k=3, ninit=5, imax0=2,
+        chbetaiinit=[1, 0, 3],
+        depth=5, strbp="_backprop",
+        calpha=6, bound=0.1, nseed=39, subcounts=4001
+    ),
+
+    (210, 0.1, "nobias"): NodeConfig(
+        alphasc=1.5, beta=0.0, iinit=0, 
+        n_qubits=8, k=4, ninit=5, imax0=2,
+        chbetaiinit=[0, 4, 2],
+        depth=5, strbp="_backprop",
+        calpha=6, bound=0.1, nseed=0, subcounts=4001
+    ),
+
+    (756, 0.1, "nobias"): NodeConfig(
+        alphasc=0.5, beta=0.0, iinit=4, 
+        n_qubits=10, k=5, ninit=5, imax0=3,
+        depth=5, strbp="_backprop",
         calpha=2, bound=0.1, nseed=7, subcounts=4001
     ),
 
-    2772: NodeConfig(
-        n_qubits=12, k=6, ninit=5, iinit=3, imax0=5,
-        alphasc=0.1,
-        beta=0.0, depth=5, strbp="_backprop",
-        calpha=2, bound=0.1, nseed=9, subcounts=4001
+#    (2772, 0.1, "nobias"): NodeConfig(
+#        n_qubits=12, k=6, ninit=5, iinit=3, imax0=5,
+#        alphasc=0.1,
+#        beta=0.0, depth=5, strbp="_backprop",
+#        calpha=2, bound=0.1, nseed=9, subcounts=4001
+#    ),
+
+    (18, 0.1, "bias_x"): NodeConfig(
+        alphasc=2.5, beta=0.2, iinit=2,
+        n_qubits=4, k=2, ninit=5, imax0=1,
+        chbetaiinit=[2, 3, 3, 4],
+        betas=[-0.1, 0.0, 0.1, 0.2],
+        depth=5, strbp="_backprop",
+        calpha=80, bound=1.0, nseed=24, subcounts=4001
     ),
 
-    10296: NodeConfig(
-        n_qubits=14, k=7, ninit=5, iinit=2, imax0=8,
-        alphasc=0.1, chbetaiinit=[2, 2, 0, 0],
-        betas=[-0.1, 0.0, 0.1, 0.1],
-        beta=0.0, depth=5, strbp="_backprop",
-        calpha=2, bound=0.1, nseed=0, subcounts=4001
+    (60, 0.1, "bias_x"): NodeConfig(
+        alphasc=1.0, beta=0.1, iinit=3, 
+        n_qubits=6, k=3, ninit=5, imax0=2,
+        chbetaiinit=[1, 0, 3],
+        depth=5, strbp="_backprop",
+        calpha=6, bound=0.1, nseed=39, subcounts=4001
+    ),
+
+    (210, 0.1, "bias_x"): NodeConfig(
+        alphasc=0.5, beta=0.0, iinit=3, 
+        n_qubits=8, k=4, ninit=5, imax0=2,
+        chbetaiinit=[0, 4, 2],
+        depth=5, strbp="_backprop",
+        calpha=6, bound=0.1, nseed=0, subcounts=4001
+    ),
+
+    (756, 0.1, "bias_x"): NodeConfig(
+        alphasc=0.5, beta=0.0, iinit=3, 
+        n_qubits=10, k=5, ninit=5, imax0=3,
+        depth=5, strbp="_backprop",
+        calpha=2, bound=0.1, nseed=7, subcounts=4001
+    ),
+
+    (18, 0.1, "bias_y"): NodeConfig(
+        alphasc=2.5, beta=0.1, iinit=1,
+        n_qubits=4, k=2, ninit=5, imax0=1,
+        chbetaiinit=[2, 3, 3, 4],
+        betas=[-0.1, 0.0, 0.1, 0.2],
+        depth=5, strbp="_backprop",
+        calpha=80, bound=1.0, nseed=24, subcounts=4001
+    ),
+
+    (60, 0.1, "bias_y"): NodeConfig(
+        alphasc=1.5, beta=0.0, iinit=4, 
+        n_qubits=6, k=3, ninit=5, imax0=2,
+        chbetaiinit=[1, 0, 3],
+        depth=5, strbp="_backprop",
+        calpha=6, bound=0.1, nseed=39, subcounts=4001
+    ),
+
+    (210, 0.1, "bias_y"): NodeConfig(
+        alphasc=0.5, beta=-0.1, iinit=3, 
+        n_qubits=8, k=4, ninit=5, imax0=2,
+        chbetaiinit=[0, 4, 2],
+        depth=5, strbp="_backprop",
+        calpha=6, bound=0.1, nseed=0, subcounts=4001
+    ),
+
+    (756, 0.1, "bias_y"): NodeConfig(
+        alphasc=0.5, beta=0.0, iinit=3, 
+        n_qubits=10, k=5, ninit=5, imax0=3,
+        depth=5, strbp="_backprop",
+        calpha=2, bound=0.1, nseed=7, subcounts=4001
     ),
 }
