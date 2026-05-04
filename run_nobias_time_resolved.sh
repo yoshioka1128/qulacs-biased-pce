0f5a239598e17dd2ed43bf8d8b8d15e07f02a2f7
Takuya Yoshioka <88071178+yoshioka1128@users.noreply.github.com>
2026-05-04 19:42:57 +0900

    update

--- run_nobias_time_resolved.sh
+++ run_nobias_time_resolved.sh
@@ -4,11 +4,14 @@ mkdir -p logs
 
 #for m in 18 60 210 756 2772; do
 #for m in 2772; do
-for m in 18; do
+#for rate in 0.2 0.3 0.4; do
+for rate in 0.5; do
+    for m in 756; do
 	for i in {11..20}; do
         echo "Running m=$m, itime=$i"
 
-        python -m src.app.main --m $m --readmode --itime $i \
-            > logs/m${m}_itime${i}.log 2>&1
+        nohup python -m src.app.main --m $m --readmode --itime $i --mode nobias --rate  $rate\
+               > logs/m${m}_itime${i}_rate${rate}_timeresolved.log 2>&1 &
+	done
     done
 done
